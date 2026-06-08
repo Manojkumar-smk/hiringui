009c5149a21196cd3c387b2a10b753ee372f4e1f
Manojkumar S <manojkumar.smk09@outlook.com>
2026-06-08 05:46:19 +0000

    hiring ui - BPA task ui

---
+++ hiringui/candidate-info/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/hr/candidateinfo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ hiringui/candidate-info/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comhr/candidate-info/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
